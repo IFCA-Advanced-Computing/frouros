0433abf7868256eb41a4f8efe801eb6f9385d7fe
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2022-04-19 10:56:55 +0200

    Refactor unsupervised KS to use TestEstimator class

--- frouros/unsupervised/ks.py
+++ frouros/unsupervised/ks.py
@@ -7,26 +7,13 @@ from sklearn.base import BaseEstimator, TransformerMixin  # type: ignore
 from sklearn.utils.validation import check_array, check_is_fitted  # type: ignore
 
 from frouros.unsupervised.exceptions import MisMatchDimensionError
+from frouros.unsupervised.base import TestEstimator
 
 
-class KSTest(BaseEstimator, TransformerMixin):
+class KSTest(BaseEstimator, TransformerMixin, TestEstimator):
     """KSTest (Kolmogorov-Smirnov test) algorithm class."""
 
-    def __init__(self) -> None:
-        """Init method."""
-        self.X_ref_ = None
-        self.test = None
-
-    @property
-    def X_ref_(self) -> Optional[np.ndarray]:  # noqa: N802
-        """Reference data property.
-
-        :return: reference data
-        :rtype: Optional[numpy.ndarray]
-        """
-        return self._X_ref_  # pylint: disable=E1101
-
-    @X_ref_.setter
+    @TestEstimator.X_ref_.setter  # type: ignore[attr-defined]
     def X_ref_(self, value: Optional[np.ndarray]) -> None:  # noqa: N802
         """Reference data setter.
 
@@ -35,54 +22,7 @@ class KSTest(BaseEstimator, TransformerMixin):
         """
         self._X_ref_ = check_array(value) if value is not None else value  # noqa: N806
 
-    @property
-    def test(self) -> Optional[List[Tuple[float, float]]]:
-        """Test results property.
-
-        :return: test results
-        :rtype: Optional[List[Tuple[float, float]]]
-        """
-        return self._test
-
-    @test.setter
-    def test(self, value: Optional[List[Tuple[float, float]]]) -> None:
-        """Test results setter.
-
-        :param value: value to be set
-        :type value: Optional[List[Tuple[float, float]]]
-        """
-        self._test = value
-
-    def fit(
-        self,
-        X: np.ndarray,  # noqa: N803
-        y: np.ndarray = None,  # pylint: disable=W0613
-    ):
-        """Fit estimator.
-
-        :param X: feature data
-        :type X: numpy.ndarray
-        :param y: target data
-        :type y: numpy.ndarray
-        :return fitted estimator
-        :rtype: self
-        """
-        self.X_ref_ = X
-        return self
-
-    def transform(
-        self,
-        X: np.ndarray,  # noqa: N803
-        y: np.ndarray = None,  # pylint: disable=W0613
-        **kwargs,
-    ):
-        """Transform values.
-
-        :param X: feature data
-        :type X: numpy.ndarray
-        :param y: target data
-        :rtype: numpy.ndarray
-        """
+    def _validation_checks(self, X: np.ndarray) -> None:  # noqa: N803
         check_is_fitted(self, attributes="X_ref_")
         X = check_array(X)  # noqa: N806
         self.X_ref_: np.ndarray
@@ -91,14 +31,15 @@ class KSTest(BaseEstimator, TransformerMixin):
                 f"Dimensions of X_ref ({self.X_ref_.shape[1]}) "
                 f"and X ({X.shape[1]}) must be equal"
             )
-        tests = []
-        for i in range(X.shape[1]):
-            test = ks_2samp(
-                data1=self.X_ref_[:, i],
-                data2=X[:, i],
-                alternative=kwargs.get("alternative", "two-sided"),
-                mode=kwargs.get("mode", "auto"),
-            )
-            tests.append((test.statistic, test.pvalue))
-        self.test = tests
-        return X
+
+    @staticmethod
+    def _statistical_test(
+        X_ref_: np.ndarray, X: np.ndarray, **kwargs  # noqa: N803
+    ) -> List[Tuple[float, float]]:
+        test = ks_2samp(
+            data1=X_ref_,
+            data2=X,
+            alternative=kwargs.get("alternative", "two-sided"),
+            mode=kwargs.get("method", "auto"),
+        )
+        return test
