6e20bb59f411ad4c400de2187d6ba7d6c122cb41
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2022-05-18 13:22:35 +0200

    Add unsupervised MMD tests

--- frouros/unsupervised/tests/conftest.py
+++ frouros/unsupervised/tests/conftest.py
@@ -32,3 +32,29 @@ def dataset() -> Tuple[np.array, np.array, np.array]:
     X_test = X[-2:, :]  # noqa: N806
 
     return X_ref, y_ref, X_test
+
+
+@pytest.fixture(scope="module")
+def multivariate_distribution_p() -> Tuple[np.ndarray, np.ndarray]:
+    """Multivariate distribution p.
+
+    :return: mean and covariance matrix of distribution p
+    :rtype: Tuple[numpy.ndarray, numpy.ndarray]
+    """
+    mean = np.ones(2)
+    cov = 2 * np.eye(2)
+
+    return mean, cov
+
+
+@pytest.fixture(scope="module")
+def multivariate_distribution_q() -> Tuple[np.ndarray, np.ndarray]:
+    """Multivariate distribution q.
+
+    :return: mean and covariance matrix of distribution q
+    :rtype: Tuple[numpy.ndarray, numpy.ndarray]
+    """
+    mean = np.zeros(2)
+    cov = 3 * np.eye(2) - 1
+
+    return mean, cov
--- frouros/unsupervised/tests/test_unsupervised.py
+++ frouros/unsupervised/tests/test_unsupervised.py
@@ -4,21 +4,22 @@ from typing import Tuple
 
 import pytest  # type: ignore
 import numpy as np  # type: ignore
+from sklearn.gaussian_process.kernels import RBF  # type: ignore
 from sklearn.linear_model import LogisticRegression  # type: ignore
 from sklearn.pipeline import Pipeline  # type: ignore
 from sklearn.preprocessing import StandardScaler  # type: ignore
 
 from frouros.unsupervised.base import UnsupervisedBaseEstimator
-from frouros.unsupervised.distance_based import EMD, PSI
+from frouros.unsupervised.distance_based import EMD, PSI, MMD
 from frouros.unsupervised.statistical_test import CVMTest, KSTest
 from frouros.unsupervised.utils import get_statistical_test
 
 
 @pytest.mark.parametrize("detector", [EMD(), PSI(), CVMTest(), KSTest()])
-def test_unsupervised_method(
+def test_univariate_test(
     dataset: Tuple[np.array, np.array, np.array], detector: UnsupervisedBaseEstimator
 ) -> None:
-    """Test unsupervised method.
+    """Test univariate method.
 
     :param dataset: Elec2 raw dataset
     :type dataset: Tuple[numpy.array, numpy.array, numpy.array]
@@ -37,3 +38,64 @@ def test_unsupervised_method(
 
     _ = pipe.predict(X=np.array([*X_test]))
     _ = get_statistical_test(estimator=pipe, detector_name="detector")
+
+
+@pytest.mark.parametrize(
+    "detector", [MMD(num_permutations=1000, kernel=RBF(), random_state=31)]
+)
+def test_distance_based_multivariate_different_distribution_test(
+    multivariate_distribution_p: Tuple[np.ndarray, np.ndarray],
+    multivariate_distribution_q: Tuple[np.ndarray, np.ndarray],
+    detector: UnsupervisedBaseEstimator,
+    num_samples: int = 500,
+) -> None:
+    """Test c multivariate different distribution method.
+
+    :param multivariate_distribution_p: mean and covariance matrix of distribution p
+    :type multivariate_distribution_p: Tuple[numpy.ndarray, numpy.ndarray]
+    :param multivariate_distribution_q: mean and covariance matrix of distribution q
+    :type multivariate_distribution_q: Tuple[numpy.ndarray, numpy.ndarray]
+    """
+    np.random.seed(seed=31)
+    X_ref = np.random.multivariate_normal(  # noqa: N806
+        *multivariate_distribution_p, size=num_samples
+    )
+    X_test = np.random.multivariate_normal(  # noqa: N806
+        *multivariate_distribution_q, size=num_samples
+    )
+
+    detector.fit(X=X_ref)
+    detector.transform(X=X_test)
+    statistic, p_value = detector.test  # type: ignore
+
+    assert np.isclose(statistic, 0.09446612)
+    assert np.isclose(p_value, 0.0)
+
+
+@pytest.mark.parametrize(
+    "detector", [MMD(num_permutations=1000, kernel=RBF(), random_state=31)]
+)
+def test_distance_based_multivariate_same_distribution_test(
+    multivariate_distribution_p: Tuple[np.ndarray, np.ndarray],
+    detector: UnsupervisedBaseEstimator,
+    num_samples: int = 500,
+) -> None:
+    """Test distance based multivariate same distribution method.
+
+    :param multivariate_distribution_p: mean and covariance matrix of distribution p
+    :type multivariate_distribution_p: Tuple[numpy.ndarray, numpy.ndarray]
+    """
+    np.random.seed(seed=31)
+    X_ref = np.random.multivariate_normal(  # noqa: N806
+        *multivariate_distribution_p, size=num_samples
+    )
+    X_test = np.random.multivariate_normal(  # noqa: N806
+        *multivariate_distribution_p, size=num_samples
+    )
+
+    detector.fit(X=X_ref)
+    detector.transform(X=X_test)
+    statistic, p_value = detector.test  # type: ignore
+
+    assert np.isclose(statistic, 0.00256109)
+    assert np.isclose(p_value, 0.894)
