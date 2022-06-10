c885866a44bf2cd7b581664e405a991232d32951
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2022-06-10 09:40:39 +0200

    Add supervised window based ADWIN

---
+++ frouros/supervised/window_based/__init__.py
@@ -0,0 +1,8 @@
+"""Supervised window based detection methods' init."""
+
+from .adwin import ADWIN, ADWINConfig
+
+__all__ = [
+    "ADWIN",
+    "ADWINConfig",
+]
---
+++ frouros/supervised/window_based/base.py
@@ -0,0 +1,42 @@
+"""Supervised window based base module."""
+
+import abc
+from typing import (  # noqa: TYP001
+    Dict,
+    Optional,
+    Tuple,
+    Union,
+)
+
+from sklearn.utils.validation import check_is_fitted  # type: ignore
+import numpy as np  # type: ignore
+
+from frouros.supervised.base import TargetDelayEstimator, SupervisedBaseConfig
+
+
+class WindowBaseConfig(SupervisedBaseConfig):
+    """Class representing a window based configuration class."""
+
+
+class WindowBasedEstimator(TargetDelayEstimator):
+    """Abstract class representing a window based estimator."""
+
+    def _prepare_update(
+        self, y: np.ndarray
+    ) -> Tuple[np.ndarray, Optional[Dict[str, float]]]:
+        check_is_fitted(self.estimator)
+        _, y_pred = self.delayed_predictions.popleft()  # noqa: N806
+        self.num_instances += y_pred.shape[0]
+
+        metrics = self._metrics_func(y_true=y, y_pred=y_pred)
+        return y_pred, metrics
+
+    @abc.abstractmethod
+    def update(self, y: np.array) -> Dict[str, Optional[Union[float, bool]]]:
+        """Update drift detector.
+
+        :param y: input data
+        :type y: numpy.ndarray
+        :return response message
+        :rtype: Dict[str, Optional[Union[float, bool]]]
+        """
