15681a76621b1f669227013c0ec10bc72ad4d0a0
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2022-07-15 17:31:04 +0200

    Move datasets tests to tests folder

--- frouros/tests/conftest.py
+++ frouros/tests/conftest.py
@@ -6,6 +6,45 @@ import pytest  # type: ignore
 import numpy as np  # type: ignore
 
 from frouros.datasets.real import Elec2
+from frouros.datasets.synthetic import SEA
+
+
+# Elec2 fixtures
+@pytest.fixture(scope="function")
+def elec2_raw() -> Elec2:
+    """Elec2 raw dataset.
+
+    :return: Elec2 raw dataset
+    :rtype: Elec2
+    """
+    dataset = Elec2()
+    dataset.download()
+    return dataset
+
+
+@pytest.fixture(scope="function")
+def elec2(elec2_raw: Elec2) -> np.ndarray:  # pylint: disable=redefined-outer-name
+    """Elec2 dataset.
+
+    :param elec2_raw: Elec2 raw dataset
+    :type elec2_raw: Elec2
+    :return: Elec2 dataset
+    :rtype: np.ndarray
+    """
+    return elec2_raw.load()
+
+
+# SEA fixtures
+@pytest.fixture(scope="function")
+def sea() -> SEA:
+    """SEA dataset generator.
+
+    :return: SEA dataset generator
+    :rtype: SEA
+    """
+    generator = SEA()
+    return generator
+
 
 
 @pytest.fixture(scope="module")
