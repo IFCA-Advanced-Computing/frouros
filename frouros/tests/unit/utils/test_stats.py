035c27eb2973d48133f70035327f23bf3842dfb8
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2023-05-11 15:39:24 +0200

    Add Mean basic unit tests

--- frouros/tests/unit/utils/test_stats.py
+++ frouros/tests/unit/utils/test_stats.py
@@ -5,7 +5,7 @@ from typing import List, Union
 import numpy as np  # type: ignore
 import pytest  # type: ignore
 
-from frouros.utils.stats import CircularMean
+from frouros.utils.stats import CircularMean, Mean
 
 
 @pytest.mark.parametrize(
@@ -31,6 +31,31 @@ def test_circular_mean(
     :type expected_mean_steps: List[Union[int, float]]
     """
     mean = CircularMean(size=size)
+
+    for value, expected_mean_step in zip(values, expected_mean_steps):
+        mean.update(value=value)
+        assert np.isclose(mean.get(), expected_mean_step)
+
+
+@pytest.mark.parametrize(
+    "values, expected_mean_steps",
+    [
+        ([5, 10, 6, 4, 14], [5.0, 7.5, 7, 6.25, 7.8]),
+        ([-5, 10, -6, 4, -14], [-5.0, 2.5, -0.33333334, 0.75, -2.2]),
+    ],
+)
+def test_mean(
+    values: List[Union[int, float]],
+    expected_mean_steps: List[Union[int, float]],
+) -> None:
+    """Test mean.
+
+    :param values: values
+    :type values: List[Union[int, float]]
+    :param expected_mean_steps: expected mean step values
+    :type expected_mean_steps: List[Union[int, float]]
+    """
+    mean = Mean()
 
     for value, expected_mean_step in zip(values, expected_mean_steps):
         mean.update(value=value)
