da108a1aa123b917ead404d4267a013f4b867495
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2023-05-11 14:17:07 +0200

    Add CircularMean basic unit tests

---
+++ frouros/tests/unit/utils/test_stats.py
@@ -0,0 +1,37 @@
+"""Test stats module."""
+
+from typing import List, Union
+
+import numpy as np  # type: ignore
+import pytest  # type: ignore
+
+from frouros.utils.stats import CircularMean
+
+
+@pytest.mark.parametrize(
+    "size, values, expected_mean_steps",
+    [
+        (1, [5, 10, 6, 4, 14], [5.0, 10.0, 6.0, 4.0, 14.0]),
+        (3, [5, 10, 6, 4, 14], [5.0, 7.5, 7.0, 6.66666667, 8.0]),
+        (6, [5, 10, 6, 4, 14], [5.0, 7.5, 7, 6.25, 7.8]),
+    ],
+)
+def test_circular_mean(
+    size: int,
+    values: List[Union[int, float]],
+    expected_mean_steps: List[Union[int, float]],
+) -> None:
+    """Test circular mean.
+
+    :param size: size value
+    :type size: int
+    :param values: values
+    :type values: List[Union[int, float]]
+    :param expected_mean_steps: expected mean step values
+    :type expected_mean_steps: List[Union[int, float]]
+    """
+    mean = CircularMean(size=size)
+
+    for value, expected_mean_step in zip(values, expected_mean_steps):
+        mean.update(value=value)
+        assert np.isclose(mean.get(), expected_mean_step)
