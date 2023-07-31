21ebb3536c647e53224bdf50583a3a98fb81daf3
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2023-07-31 12:28:18 +0200

    Add HINormalizedComplement API example

--- frouros/detectors/data_drift/batch/distance_based/hi_normalized_complement.py
+++ frouros/detectors/data_drift/batch/distance_based/hi_normalized_complement.py
@@ -11,13 +11,29 @@ from frouros.detectors.data_drift.batch.distance_based.base import (
 
 
 class HINormalizedComplement(BaseDistanceBasedBins):
-    """HI (Histogram intersection) normalized complement [swain1991color]_ detector.
+    """HI (Histogram intersection) normalized complement [1]_ detector.
+
+    :param num_bins: number of bins in which to divide probabilities, defaults to 10
+    :type num_bins: int
+    :param callbacks: callbacks, defaults to None
+    :type callbacks: Optional[Union[BaseCallbackBatch, List[BaseCallbackBatch]]]
 
     :References:
 
-    .. [swain1991color] Swain, M. J., and D. H. Ballard.
+    .. [1] Swain, M. J., and D. H. Ballard.
         "Color Indexing International Journal of Computer
         Vision 7." (1991): 11-32.
+
+    :Example:
+
+    >>> from frouros.detectors.data_drift import HINormalizedComplement
+    >>> import numpy as np
+    >>> np.random.seed(seed=31)
+    >>> X = np.random.normal(loc=0, scale=1, size=100)
+    >>> Y = np.random.normal(loc=1, scale=1, size=100)
+    >>> detector = HINormalizedComplement(num_bins=20)
+    >>> _ = detector.fit(X=X)
+    >>> result, _ = detector.compare(X=Y)
     """
 
     def __init__(
@@ -25,13 +41,6 @@ class HINormalizedComplement(BaseDistanceBasedBins):
         num_bins: int = 10,
         callbacks: Optional[Union[BaseCallbackBatch, List[BaseCallbackBatch]]] = None,
     ) -> None:
-        """Init method.
-
-        :param num_bins: number of bins in which to divide probabilities
-        :type num_bins: int
-        :param callbacks: callbacks
-        :type callbacks: Optional[Union[BaseCallbackBatch, List[BaseCallbackBatch]]]
-        """
         super().__init__(
             statistical_method=self._hi_normalized_complement,
             statistical_kwargs={
