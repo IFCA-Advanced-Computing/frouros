e451194b18b82edac3044807cb98385334757df5
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2023-07-31 19:05:23 +0200

    Add GeometricMovingAverage API example

--- frouros/detectors/concept_drift/streaming/change_detection/geometric_moving_average.py
+++ frouros/detectors/concept_drift/streaming/change_detection/geometric_moving_average.py
@@ -10,30 +10,27 @@ from frouros.detectors.concept_drift.streaming.change_detection.base import (
 class GeometricMovingAverageConfig(BaseCUSUMConfig, AlphaConfig):
     """Geometric Moving Average [robertst1959control]_ configuration.
 
+    :param alpha: forgetting factor value, defaults to 0.99
+    :type alpha: float
+    :param lambda_: delta value, defaults to 1.0
+    :type lambda_: float
+    :param min_num_instances: minimum numbers of instances to start looking for changes, defaults to 30
+    :type min_num_instances: int
+
     :References:
 
     .. [robertst1959control] Roberts, S. W.
         “Control Chart Tests Based on Geometric Moving Averages.”
         Technometrics, vol. 1, no. 3, 1959, pp. 239–50.
         JSTOR, https://doi.org/10.2307/1266443.
-    """
+    """  # noqa: E501
 
-    def __init__(
+    def __init__(  # noqa: D107
         self,
         alpha: float = 0.99,
         lambda_: float = 1.0,
         min_num_instances: int = 30,
     ) -> None:
-        """Init method.
-
-        :param alpha: forgetting factor value
-        :type alpha: float
-        :param lambda_: delta value
-        :type lambda_: float
-        :param min_num_instances: minimum numbers of instances
-        to start looking for changes
-        :type min_num_instances: int
-        """
         BaseCUSUMConfig.__init__(
             self, lambda_=lambda_, min_num_instances=min_num_instances
         )
@@ -43,13 +40,34 @@ class GeometricMovingAverageConfig(BaseCUSUMConfig, AlphaConfig):
 class GeometricMovingAverage(BaseCUSUM):
     """Geometric Moving Average [robertst1959control]_ detector.
 
+    :param config: configuration object of the detector
+    :type config: GeometricMovingAverageConfig
+    :param callbacks: callbacks, defaults to None
+    :type callbacks: Optional[Union[BaseCallbackStreaming, List[BaseCallbackStreaming]]]
+
     :References:
 
     .. [robertst1959control] Roberts, S. W.
         “Control Chart Tests Based on Geometric Moving Averages.”
         Technometrics, vol. 1, no. 3, 1959, pp. 239–50.
         JSTOR, https://doi.org/10.2307/1266443.
-    """
+
+        :Example:
+
+    >>> from frouros.detectors.concept_drift import GeometricMovingAverage, GeometricMovingAverageConfig
+    >>> import numpy as np
+    >>> np.random.seed(seed=31)
+    >>> dist_a = np.random.normal(loc=0.2, scale=0.01, size=1000)
+    >>> dist_b = np.random.normal(loc=0.8, scale=0.04, size=1000)
+    >>> stream = np.concatenate((dist_a, dist_b))
+    >>> detector = GeometricMovingAverage(config=GeometricMovingAverageConfig(lambda_=0.1))
+    >>> for i, value in enumerate(stream):
+    ...     _ = detector.update(value=value)
+    ...     if detector.drift:
+    ...         print(f"Change detected at index {i}")
+    ...         break
+    Change detected at index 1018
+    """  # noqa: E501
 
     config_type = GeometricMovingAverageConfig  # type: ignore
 
